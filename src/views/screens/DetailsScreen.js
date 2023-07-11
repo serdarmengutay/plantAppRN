068e568087ef03167cd8833c04d270607271452e
Serdar Mengütay <serdarmngty03@gmail.com>
2023-07-11 14:01:39 +0300

    plant app created and home screen coded

---
+++ src/views/screens/DetailsScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { SafeAreaView, View, Text,  } from 'react-native'
+
+const DetailsScreen = ({ navigation, route }) => {
+  return (
+    <SafeAreaView>
+    </SafeAreaView>
+  )
+}
+
+export default DetailsScreen
